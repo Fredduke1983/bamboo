08e3e733fd73b90246b80646f1a2a7bf32796c14
Nechai_Ihor <fredduke1983@gmail.com>
2023-08-08 11:24:39 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -14,17 +14,10 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+// import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        // svgr options
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     open: "/bamboo",
     port: 8000,
