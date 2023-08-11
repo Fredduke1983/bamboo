31d884ada1827fcbdd07735518e70329571c7c3b
Nechai_Ihor <fredduke1983@gmail.com>
2023-08-12 01:35:55 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/bamboo/dist",
+  base: "/",
   plugins: [react()],
   server: {
     open: "/bamboo",
