a7306837ac7efa1342ba3a19633e5670a329b8bd
Nechai_Ihor <fredduke1983@gmail.com>
2023-08-13 23:42:19 +0300

    UP: testimonial section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,21 +1,34 @@
 import bg from "../../img/bamboo-leaf.png";
-import { TestimonialsWrapper } from "./Testimonials.styled";
+import customer from "../../img/9379.jpg";
+import {
+  CustomerImg,
+  CustomerName,
+  TestimonialBg,
+  TestimonialDescription,
+  TestimonialStyle,
+  TestimonialSubTitle,
+  TestimonialTitle,
+  TestimonialWrapper,
+} from "./Testimonials.styled";
 
 export const Testimonials = () => {
   return (
-    <TestimonialsWrapper>
-      <div>
-        <h2>Testimonial</h2>
-        <h3>What Our Customer Saying?</h3>
-        <p>
-          Simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been.
-        </p>
-        <p>Name</p>
-      </div>
+    <TestimonialWrapper>
+      <TestimonialStyle>
+        <TestimonialTitle>Testimonial</TestimonialTitle>
+        <TestimonialSubTitle>What Our Customer Saying?</TestimonialSubTitle>
+        <CustomerImg src={customer} alt="foto customer" />
+        <CustomerName>Anna</CustomerName>
+        <TestimonialDescription>
+          I&apos;m delighted to share a positive experience regarding my
+          purchase of bamboo from the &#34;Sparrows&#34; seller. The variety was
+          impressive, and the service was top-notch. The plant arrived in
+          excellent condition - healthy and well-cared-for. Many thanks for your
+          professionalism and wonderful plant selection!
+        </TestimonialDescription>
+      </TestimonialStyle>
 
-      <img src={bg} />
-    </TestimonialsWrapper>
+      <TestimonialBg src={bg} alt="background" />
+    </TestimonialWrapper>
   );
 };
--- src/components/Testimonials/Testimonials.styled.js
+++ src/components/Testimonials/Testimonials.styled.js
@@ -1,11 +1,68 @@
 import styled from "styled-components";
+import { colors } from "../../utils/_variables";
 
-const TestimonialsWrapper = styled.section`
+const TestimonialWrapper = styled.section`
   display: flex;
   justify-content: space-between;
-  img {
-    /* width: 100%; */
-  }
+  margin-top: 120px;
+
+  background-color: #cccccc50;
 `;
 
-export { TestimonialsWrapper };
+const TestimonialStyle = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 60px;
+`;
+
+const TestimonialTitle = styled.h2`
+  font-family: "Yellowtail", cursive;
+  font-size: 26px;
+  color: ${colors.titleColor};
+
+  margin-bottom: 8px;
+`;
+const TestimonialSubTitle = styled.h3`
+  font-family: "Roboto", sans-serif;
+  font-weight: 900;
+  font-size: 36px;
+  color: ${colors.mainTextColor};
+  margin-bottom: 60px;
+`;
+
+const CustomerImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  box-shadow: 0px 0px 9px 6px #aacccc;
+  margin-bottom: 6px;
+`;
+
+const CustomerName = styled.p`
+  font-family: "Roboto", sans-serif;
+  font-weight: 600;
+  font-size: 25px;
+  color: ${colors.mainTextColor};
+  margin-bottom: 18px;
+`;
+
+const TestimonialDescription = styled.p`
+  width: 500px;
+  font-size: 18px;
+
+  margin: 0 auto;
+  color: ${colors.textColor};
+`;
+
+const TestimonialBg = styled.img``;
+
+export {
+  TestimonialWrapper,
+  TestimonialStyle,
+  TestimonialTitle,
+  TestimonialSubTitle,
+  CustomerImg,
+  TestimonialDescription,
+  CustomerName,
+  TestimonialBg,
+};
