a6d79b5482b272db19be816b3841b70be54f42b2
Nechai_Ihor <fredduke1983@gmail.com>
2023-08-15 14:13:05 +0300

    try

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/bamboo",
+  // base: "/bamboo",
   plugins: [react()],
   server: {
     // open: "/bamboo",
